603d753873a4a8dc94f24d07b2fe7daff9165474
Bakresh <bakresh@gmail.com>
2022-10-23 19:40:55 -0400

    Add a function and event listener for each digit button

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+const digits = Array.from(document.querySelectorAll(".digit"));
+digits.forEach(digit => digit.addEventListener('click', pressDigit));
+
 function add(a,b) {
     return a + b;
 }
@@ -21,4 +24,13 @@ function operate(operator,a,b) {
     if (operator === "subtract") return subtract(aInt,bInt);
     if (operator === "multiply") return multiply(aInt,bInt);
     if (operator === "divide") return divide(aInt,bInt);
+}
+
+function pressDigit(e) {
+    const numbers = document.querySelector(".numbers");
+    if (numbers.textContent == 0) {
+        numbers.textContent = e.target.textContent;
+    } else {
+        numbers.textContent += e.target.textContent;
+    }
 }
